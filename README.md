70c5d359156ca468fd06c5fbbeccf75ea602ea9a
pramuditya.anantanur <pramuditya.anantanur@gdn-commerce.com>
2019-11-13 19:10:21 +0700

    Basic Setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spring Boot 2 Example Documentation
+
+## Table of Content
+- Basic Setup
+
+
+### Basic Setup
+- Initialize Spring Boot 2 Project
+    - Spring Data JPA
+    - Spring Web
+    - Spring Boot DevTools
+    - Lombok
+
---
+++ src/test/java/com/nantaaditya/example/springboot2/SpringBoot2ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nantaaditya.example.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SpringBoot2ApplicationTests {
+
+  @Test void contextLoads() {
+  }
+
+}
